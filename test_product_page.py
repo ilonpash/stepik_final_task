251c0c8158cdd686c67fce5aa34d8528e575d8c3
ilonpash <ilonapashykian@gmail.com>
2022-02-05 20:51:22 +0200

    added setup function with registration and mark some cases as need_review

--- test_product_page.py
+++ test_product_page.py
@@ -5,6 +5,34 @@ import pytest
 import time
 
 
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+        page = LoginPage(browser, link)
+        page.open()
+        email = str(time.time()) + "@fakemail.org"
+        password = str("123fghgch7ukj")
+        page.register_new_user(email, password)
+        page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        page.solve_quiz_and_get_code()
+        page.should_be_same_name(page.text_of_book_name())
+        page.should_be_same_price(page.text_of_book_price())
+
+
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -79,30 +107,3 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page.should_be_empty_basket()
     basket_page.should_be_empty_basket_message_text()
 
-
-class TestUserAddToBasketFromProductPage():
-    @pytest.fixture(scope="function", autouse=True)
-    def setup(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/accounts/login/"
-        page = LoginPage(browser, link)
-        page.open()
-        email = str(time.time()) + "@fakemail.org"
-        password = str("123fghgch7ukj")
-        page.register_new_user(email, password)
-        page.should_be_authorized_user()
-
-    def test_user_cant_see_success_message(self, browser):
-        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
-        page = ProductPage(browser, link)
-        page.open()
-        page.should_not_be_success_message()
-
-    @pytest.mark.need_review
-    def test_user_can_add_product_to_basket(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
-        page = ProductPage(browser, link)
-        page.open()
-        page.add_to_basket()
-        page.solve_quiz_and_get_code()
-        page.should_be_same_name(page.text_of_book_name())
-        page.should_be_same_price(page.text_of_book_price())
